76b981db2becc8a3770d1c6922bc5b0125b57a49
y-kato19321 <yoichi_kato@sobal.co.jp>
2020-10-21 13:35:49 +0900

    10月アプリ実装

--- app/src/main/java/com/example/ble_communicator/data/NumberPickerData.java
+++ app/src/main/java/com/example/ble_communicator/data/NumberPickerData.java
@@ -1,5 +1,7 @@
 package com.example.ble_communicator.data;
 
+import com.example.ble_communicator.R;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,13 +11,13 @@ public class NumberPickerData {
     private static String plamaizero = "±0.0";
 
     //温度誤差の設定
-    final String[] np_calibration_list = {
+    final String[] spinner_offsettemp_list = {
             "+6.0", "+5.5", "+5.0", "+4.5", "+4.0", "+3.5", "+3.0", "+2.5", "+2.0", "+1.5", "+1.0", "+0.5",
             "±0.0"
     };
 
     //高温閾値の設定
-    final String[] np_hightemperaturethreshold_list = {
+    final String[] spinner_thresholddegree_list = {
             "40.5", "40.4", "40.3", "40.2", "40.1", "40.0",
             "39.9", "39.8", "39.7", "39.6", "39.5", "39.4", "39.3", "39.2", "39.1", "39.0",
             "38.9", "38.8", "38.7", "38.6", "38.5", "38.4", "38.3", "38.2", "38.1", "38.0",
@@ -23,46 +25,41 @@ public class NumberPickerData {
             "36.9", "36.8"
     };
 
-    final String[] np_referencetemperatureupdatetime_list = {
+
+    final String[] spinner_updatetime_list = {
             "60", "50", "40", "30", "20", "10"
     };
 
+    final String[] spinner_detecmode_list = {
+            "手首", "顔1人", "顔2人"
+    };
 
 
-    //キャリブレーションNumberPickerに設定する個数を返す
-    public int getCalibralinNPMaxvalue() {
-        return np_calibration_list.length - 1;
-    }
-
-    //キャリブレーションNumberPickerに設定する文字列リストを返す
-    public String[] getCalibrationListContents() {
-        return np_calibration_list;
+    public String[] getOffsetTempList() {
+        return spinner_offsettemp_list;
     }
 
     /**
-     * キャリブレーションNumberPickerの、指定値に対応するインデックスを返す
-     * @param calibration キャリブレーション値
-     * @return　キャリブレーション値に対応するリストのインデックス
+     * 指定値に対応するインデックスを返す
+     * @return　値に対応するリストのインデックス
      */
-    public int getCalibrationNPIndexOf(double calibration) {
+    public int getOffsetTempSpinnerIndex(double OffsetTemp) {
 
-        //キャリブレーション値の文字列化
-        String strcalib = String.valueOf(calibration);
+        String strOffsetTemp = String.valueOf(OffsetTemp);
 
         //文字列に記号を追加
-        if (calibration == 0.0) {
-            strcalib = plamaizero;
-        } else if (calibration > 0) {
-            strcalib = preplus + strcalib;
+        if (OffsetTemp == 0.0) {
+            strOffsetTemp = plamaizero;
+        } else if (OffsetTemp > 0) {
+            strOffsetTemp = preplus + strOffsetTemp;
         }
-        List<String> list = Arrays.asList(np_calibration_list); //配列をList型オブジェクトに変換
-        return list.indexOf(strcalib);
+        List<String> list = Arrays.asList(spinner_offsettemp_list); //配列をList型オブジェクトに変換
+        return list.indexOf(strOffsetTemp);
     }
 
-    public double getCalibrationIndexDoublenum(int index) {
-
+    public double getOffsetValue(int index) {
         //indexの位置の文字列を取得する＝ユーザが設定した値
-        String str = np_calibration_list[index];
+        String str = spinner_offsettemp_list[index];
 
         double value = 0.0;
 
@@ -78,29 +75,20 @@ public class NumberPickerData {
     }
 
     /**
-     * 高温閾値NumberPickerに設定する個数を返す
-     * @return　リスト個数
-     */
-    public int getHighTemperatureThresholdMaxvalue() {
-        return np_hightemperaturethreshold_list.length - 1;
-    }
-
-    /**
      * 高温閾値NumberPickerに設定する文字列リストを返す
      * @return
      */
-    public String[] getHighTemperatureThresholdListContents() {
-        return np_hightemperaturethreshold_list;
+    public String[] getThresholddegreeList() {
+        return spinner_thresholddegree_list;
     }
 
     /**
      * 高温閾値NumberPickerの、指定値に対応するインデックスを返す
-     * @param hightemperaturethreshold
      * @return　高温閾値リスト
      */
-    public int getHighTemperatureThresholdNPIndexOf(double hightemperaturethreshold) {
-        String strht = String.valueOf(hightemperaturethreshold);
-        List<String> list = Arrays.asList(np_hightemperaturethreshold_list); //配列をList型オブジェクトに変換
+    public int getThresholddegreeSpinnerIndex(double thresholddegree) {
+        String strht = String.valueOf(thresholddegree);
+        List<String> list = Arrays.asList(spinner_thresholddegree_list); //配列をList型オブジェクトに変換
         return list.indexOf(strht);
     }
 
@@ -109,48 +97,195 @@ public class NumberPickerData {
      * @param index
      * @return
      */
-    public double getHighTemperatureThresholdNPIndexDoublenum(int index) {
+    public double getThresholddegreeValue(int index) {
         //indexの位置の文字列を取得する＝ユーザが設定した値
-        String str = np_hightemperaturethreshold_list[index];
+        String str = spinner_thresholddegree_list[index];
         return Double.parseDouble(str);
     }
 
-    /**
-     * 基準温度更新時間NumberPickerに設定する個数を返す
-     * @return　リスト個数
-     */
-    public int getReferenceTemperatureUpdatetimeMaxvalue() {
-        return np_referencetemperatureupdatetime_list.length - 1;
-    }
 
     /**
-     * 基準温度更新時間NumberPickerに設定する文字列リストを返す
+     * 基準温度更新時間に設定する文字列リストを返す
      * @return
      */
-    public String[] getReferenceTemperatureUpdatetimeListContents() {
-        return np_referencetemperatureupdatetime_list;
+    public String[] getUpdateTimeList() {
+        return spinner_updatetime_list;
     }
 
     /**
-     * 高温閾値NumberPickerの、指定値に対応するインデックスを返す
-     * @param referenceTemperatureUpdateTime
+     * 基準温度更新時間の指定値に対応するインデックスを返す
      * @return　高温閾値リスト
      */
-    public int getReferenceTemperatureUpdatetimeNPIndexOf(int referenceTemperatureUpdateTime) {
-        String strht = String.valueOf(referenceTemperatureUpdateTime);
-        List<String> list = Arrays.asList(np_referencetemperatureupdatetime_list); //配列をList型オブジェクトに変換
+    public int getUpdatetimeSpinnerIndex(int updateTime) {
+        String strht = String.valueOf(updateTime);
+        List<String> list = Arrays.asList(spinner_updatetime_list); //配列をList型オブジェクトに変換
         return list.indexOf(strht);
     }
 
     /**
-     * 高温閾値NumberPickerの位置のDouble値を返す
+     * 基準温度更新時間の位置のDouble値を返す
      * @param index
      * @return
      */
-    public int getReferenceUDTimeChangeIntnum(int index) {
+    public int getUpdateTimeValue(int index) {
         //indexの位置の文字列を取得する＝ユーザが設定した値
-        String str = np_referencetemperatureupdatetime_list[index];
+        String str = spinner_updatetime_list[index];
         return Integer.valueOf(str);
+    }
+
+
+    public class Inner2 {
+        public String str;
+        public int value;
+
+        public Inner2(String _str, int _value)
+        {
+            str = _str;
+            value = _value;
+        }
+    }
+
+    private final Inner2[] mDetecmodeList = {
+            new Inner2("手首", 0),
+            new Inner2("顔1人", 1),
+            new Inner2("顔2人", 2)
+    };
+
+    public String[] getDetecModeList() {
+        int legth = mDetecmodeList.length;
+        String[] list = new String[legth];
+        for(int i = 0; i < legth; i++){
+            list[i] = mDetecmodeList[i].str;
+        }
+
+        return list;
+    }
+
+    public int getDetecModeSpinnerIndex(int detecMode){
+        int index = 0;
+        int size = mDetecmodeList.length;
+        for(int i = 0; i < size; i++) {
+            if(mDetecmodeList[i].value == detecMode)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public int getDetecModeValue(int index) {
+        int value = 0;
+        if(     (0 < index)
+            &&  (index < mDetecmodeList.length))
+        {
+            value = mDetecmodeList[index].value;
+        }
+
+        return value;
+    }
+
+
+
+    private int getValue(Inner[] innerList, int id){
+        int value = 0;
+
+        for(int i = 0; i < innerList.length; i++)
+        {
+            if(innerList[i].id == id)
+            {
+                value = innerList[i].value;
+                break;
+            }
+        }
+        return value;
+    }
+
+    private int getId(Inner[] innerList, int value){
+        int id = 0;
+
+        for(int i = 0; i < innerList.length; i++)
+        {
+            if(innerList[i].value == value)
+            {
+                id = innerList[i].id;
+                break;
+            }
+        }
+        return id;
+    }
+
+    public class Inner {
+        public int id;
+        public int value;
+
+        public Inner(int _id, int _value)
+        {
+            id = _id;
+            value = _value;
+        }
+    }
+
+    //////////////////////////////////////////////////
+    //private final Inner[] mLogModeList = {
+    //        new Inner(R.id.radiobutton_logmode_hide, 0),
+    //        new Inner(R.id.radiobutton_logmode_open, 1)
+    //};
+
+    //public int getLogMode(int id){
+    //    return getValue(mLogModeList, id);
+    //}
+
+    //public int getRadioLogId(int value) {
+    //    return getId(mLogModeList, value);
+    //}
+    //////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////
+/*    private  final Inner[] mTemperatureModeList = {
+            new Inner(R.id.radiobutton_detecmode_arm, 0),
+            new Inner(R.id.radiobutton_detecmode_face1, 1),
+            new Inner(R.id.radiobutton_detecmode_face2, 2)
+    };
+
+    public int getTemperatureMode(int id){
+        return getValue(mTemperatureModeList, id);
+    }
+
+    public int getRadioTemperatureModeId(int value){
+        return getId(mTemperatureModeList, value);
+    }
+*/
+
+
+    //////////////////////////////////////////////////
+    private  final Inner[] mTemperatureUnitList = {
+            new Inner(R.id.radiobutton_temperature_unit_celsius, 0),
+            new Inner(R.id.radiobutton_temperature_unit_fahrenheit, 1)
+    };
+
+    public int getTemperatureUnit(int id) {
+        return getValue(mTemperatureUnitList, id);
+    }
+
+    public int getRadioTemperatureUnitId(int value) {
+        return getId(mTemperatureUnitList, value);
+    }
+
+    //////////////////////////////////////////////////
+    private  final Inner[] mThermographyMode = {
+            new Inner(R.id.radiobutton_thermographymode_hide, 0),
+            new Inner(R.id.radiobutton_thermographymode_open, 1)
+    };
+
+
+    public int getThermographyMode(int id) {
+        return  getValue(mThermographyMode, id);
+    }
+
+    public int getRadioThermographyModeId(int value) {
+        return getId(mThermographyMode, value);
     }
 
 }
